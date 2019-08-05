954a4fa941cc2be08eb929ae7cd529ec567b8f78
xjhome <junxstudio@sina.com>
2019-08-05 22:01:28 +0800

    有关tf.data API的学习使用

--- tensorflow_stu/train/training.py
+++ tensorflow_stu/train/training.py
@@ -18,34 +18,6 @@ tf.enable_eager_execution()
 print('is eager executing: ', tf.executing_eagerly())
 
 
-dataset1 = tf.data.Dataset.from_tensor_slices(tf.random_uniform([4, 10]))
-print(dataset1.output_types)  # ==> "tf.float32"
-print(dataset1.output_shapes)  # ==> "(10,)"
-
-dataset2 = tf.data.Dataset.from_tensor_slices(
-   (tf.random_uniform([4]),
-    tf.random_uniform([4, 100], maxval=100, dtype=tf.int32)))
-print(dataset2.output_types)  # ==> "(tf.float32, tf.int32)"
-print(dataset2.output_shapes)  # ==> "((), (100,))"
-
-dataset3 = tf.data.Dataset.zip((dataset1, dataset2))
-print(dataset3.output_types)  # ==> (tf.float32, (tf.float32, tf.int32))
-print(dataset3.output_shapes)  # ==> "(10, ((), (100,)))"
-
-
-dataset1 = dataset1.map(lambda x: ...)
-
-dataset2 = dataset2.flat_map(lambda x, y: ...)
-
-# Note: Argument destructuring is not available in Python 3.
-dataset3 = dataset3.filter(lambda x, (y, z): ...)
-
-
-
-
->>> 创建迭代器
-
-
 if __name__ == '__main__':
     (mnist_images, mnist_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
 
