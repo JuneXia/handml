9b53fa5669028917f9ed4194d1ef3984565b10b0
xjhome <junxstudio@sina.com>
2019-08-08 22:09:18 +0800

    熟悉Dataset.map、批处理等操作

--- tensorflow_stu/datasets/experiment_api_stu.py
+++ tensorflow_stu/datasets/experiment_api_stu.py
@@ -2,6 +2,10 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.utils import to_categorical
+import os
+
+
+DEBUG = True
 
 
 # 数据集结构
@@ -166,7 +170,8 @@ def demo6():
             except tf.errors.OutOfRangeError:
                 break
 
-
+# 使用 Dataset.map() 预处理数据
+## 解析 tf.Example 协议缓冲区消息
 def demo7():
     dataset1 = tf.data.Dataset.from_tensor_slices(tf.random_uniform([4, 10]))
     dataset2 = tf.data.Dataset.from_tensor_slices((tf.random_uniform([4]), tf.random_uniform([4, 100])))
@@ -183,24 +188,126 @@ def demo7():
         print(sess.run(next3))
 
     # Create saveable object from iterator.
-    saveable = tf.contrib.data.make_saveable_from_iterator(iterator)
+    # saveable = tf.contrib.data.make_saveable_from_iterator(iterator)
+    saveable = tf.data.experimental.make_saveable_from_iterator(iterator)
 
     # Save the iterator state by adding it to the saveable objects collection.
     tf.add_to_collection(tf.GraphKeys.SAVEABLE_OBJECTS, saveable)
     saver = tf.train.Saver()
 
-    为什么需要保存迭代器状态
+    ckpt_save_path = './save_model'
     with tf.Session() as sess:
-        saver.save(path_to_checkpoint)
+        saver.save(sess, ckpt_save_path)
 
     # Restore the iterator state.
     with tf.Session() as sess:
-        saver.restore(sess, path_to_checkpoint)
+        saver.restore(sess, ckpt_save_path)
 
 
+def get_dataset(data_path):
+    images_path = []
+    images_label = []
+    images = os.listdir(data_path)
+    images.sort()
+    if DEBUG:
+        images = images[0:2]
+    for i, image in enumerate(images):
+        imgs = os.listdir(os.path.join(data_path, image))
+        if DEBUG:
+            imgs = imgs[0:1000]
+        images_path.extend([os.path.join(data_path, image, img) for img in imgs])
+        images_label.extend([i] * len(imgs))
+
+    return images_path, images_label
+
+
+## 解码图片数据并调整其大小，批处理，随机重排
+def demo8():
+    # Reads an image from a file, decodes it into a dense tensor, and resizes it
+    # to a fixed shape.
+    def _parse_function(filename, label):
+        image_string = tf.read_file(filename)
+        image_decoded = tf.image.decode_jpeg(image_string)
+        image_resized = tf.image.resize_images(image_decoded, [28, 28])
+        return image_resized, label
+
+    data_path = '/home/xiaj/res/mnist'
+    images_path, images_label = get_dataset(data_path)
+
+    # A vector of filenames.
+    filenames = tf.constant(images_path)
+
+    # `labels[i]` is the label for the image in `filenames[i].
+    labels = tf.constant(images_label)
+
+    dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+    dataset = dataset.map(_parse_function)
+    dataset = dataset.shuffle(buffer_size=2000, seed=666, reshuffle_each_iteration=True)
+    dataset = dataset.repeat(5)
+    dataset = dataset.batch(100)
+
+    iterator = dataset.make_initializable_iterator()
+    next_element = iterator.get_next()
+
+    with tf.Session() as sess:
+        sess.run(iterator.initializer)
+        while True:
+            try:
+                images, labels = sess.run(next_element)
+                print('images.shape={}, labels.shape={}, labels={}'.format(images.shape, labels.shape, labels))
+            except tf.errors.OutOfRangeError as e:
+                # 迭代完后，如果还想要继续从头迭代，可以再次sess.run(iterator.initializer)即可。
+                print(e)
+
+
+## 使用 tf.py_func() 应用任意 Python 逻辑
+def demo9():
+    import cv2
+
+    # Use a custom OpenCV function to read the image, instead of the standard
+    # TensorFlow `tf.read_file()` operation.
+    def _read_py_function(filename, label):
+        print(filename, label)
+        image_decoded = cv2.imread(filename.decode(), cv2.IMREAD_GRAYSCALE)
+        return image_decoded, label
+
+    # Use standard TensorFlow operations to resize the image to a fixed shape.
+    def _resize_function(image_decoded, label):
+        image_decoded.set_shape([None, None, None])
+        image_resized = tf.image.resize_images(image_decoded, [28, 28])
+        return image_resized, label
+
+    data_path = '/home/xiaj/res/mnist'
+    images_path, images_label = get_dataset(data_path)
+
+    dataset = tf.data.Dataset.from_tensor_slices((images_path, images_label))
+    dataset = dataset.map(
+        lambda filename, label: tuple(tf.py_function(
+            _read_py_function, [filename, label], [tf.uint8, label.dtype])))
+    dataset = dataset.map(_resize_function)
+
+    dataset = dataset.shuffle(buffer_size=2000, seed=666, reshuffle_each_iteration=True)
+    dataset = dataset.repeat(5)
+    dataset = dataset.batch(100)
+
+    iterator = dataset.make_initializable_iterator()
+    next_element = iterator.get_next()
+
+    with tf.Session() as sess:
+        sess.run(iterator.initializer)
+        while True:
+            try:
+                images, labels = sess.run(next_element)
+                print('images.shape={}, labels.shape={}, labels={}'.format(images.shape, labels.shape, labels))
+            except tf.errors.OutOfRangeError as e:
+                # 迭代完后，如果还想要继续从头迭代，可以再次sess.run(iterator.initializer)即可。
+                print(e)
+
+
+demo9 没有调通
 
 if __name__ == '__main__':
-    demo7()
+    demo9()
 
     print('debug')
 
